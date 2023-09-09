82fdc6f599e5ba4db37be58ea1a4651685ccef35
weilrfr <boyarkinid3@gmail.com>
2023-09-09 18:01:23 +0600

    da

--- src/main.js
+++ src/main.js
@@ -15,18 +15,18 @@ import PrimeVue from 'primevue/config';
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css';
 
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAUwrnBAY10e8LsyFg6A8RrnYVKDRJPW0A",
-    authDomain: "learn-vue-53df4.firebaseapp.com",
-    projectId: "learn-vue-53df4",
-    storageBucket: "learn-vue-53df4.appspot.com",
-    messagingSenderId: "214163821412",
-    appId: "1:214163821412:web:d2cc7d5109ec3b09012071"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAUwrnBAY10e8LsyFg6A8RrnYVKDRJPW0A",
+//     authDomain: "learn-vue-53df4.firebaseapp.com",
+//     projectId: "learn-vue-53df4",
+//     storageBucket: "learn-vue-53df4.appspot.com",
+//     messagingSenderId: "214163821412",
+//     appId: "1:214163821412:web:d2cc7d5109ec3b09012071"
+//   };
 
-initializeApp(firebaseConfig);
+// initializeApp(firebaseConfig);
 
 const app = createApp(App)
 
